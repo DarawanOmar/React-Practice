3a6d91a7d2586ada18997b362b168fd72bb37738
Darawan Omar <103837458+DarawanOmar@users.noreply.github.com>
2023-07-01 15:05:58 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import RunReactQuery from "./componets/ReactQuery/RunReactQuery";
+//import RunReactQuery from "./componets/ReactQuery/RunReactQuery";
 // import RunRedux from "./componets/redux/RunRedux";
 // import RunUseContext from "./componets/useContext/RunUseContext";
-// import RunUseForm from './componets/FormValidation/RunUseForm'
+ import RunUseForm from './componets/FormValidation/RunUseForm'
 function App() {  
 
   return (
@@ -11,8 +11,8 @@ function App() {
 
       {/* <RunRedux/> */}
       {/* <RunUseContext/> */}
-      {/* <RunUseForm /> */}
-      <RunReactQuery/>
+      <RunUseForm /> 
+      {/*<RunReactQuery/>*/}
     </div>
 
   );
